60c1dff3f235e03c8ee965872bd113a4717fe607
zchen <zchen@comscore.com>
2014-09-12 16:19:02 -0400

    makeCacheMatrix
    Implemented the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse
+## 4.get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv<- NULL
+    #setMatrix is the function to set the value of the matrix
+    setMatrix <- function(y){
+        x<<-y
+        inv<<-NULL
+    }
+    #getMatrix is the function to get the value of the matrix
+    getMatrix <-function() x
+    
+    #setInverse is the function to set the value of the inverse of the matrix
+    setInverse <- function(inverse) inv<<-inverse
+    
+    #getInverse is the function to retrieve the catched inverse if the matrix
+    getInverse <- function() inv
+    
+    #Return a list if the the functions created above
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
